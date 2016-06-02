fd585ec4d7937001cfaa4ebfd46f5cbba3621a71
andrey019 <andrey019@gmail.com>
2016-06-02 19:10:48 +0300

    commit

---
+++ src/HomeWork/Lesson3/HTTPServer/CacheLifeController.java
@@ -0,0 +1,40 @@
+package HomeWork.Lesson3.HTTPServer;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+class CacheLifeController extends Thread {
+    private ConcurrentHashMap<String, byte[]> map;
+    private HashMap<String, Long> timeMap;
+    private int maximumLifeTime = 60000;    // in milliseconds
+
+    CacheLifeController(ConcurrentHashMap<String, byte[]> map) {
+        this.map = map;
+        timeMap = new HashMap<>();
+    }
+
+    public void run() {
+        while (!isInterrupted()) {
+            checkCache();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void checkCache() {
+        for (Map.Entry<String, byte[]> entry : map.entrySet()) {
+            if (!timeMap.containsKey(entry.getKey())) {
+                timeMap.put(entry.getKey(), System.currentTimeMillis());
+            } else {
+                if ( (System.currentTimeMillis() - timeMap.get(entry.getKey())) > maximumLifeTime ) {
+                    timeMap.remove(entry.getKey());
+                    map.remove(entry.getKey());
+                }
+            }
+        }
+    }
+}
---
+++ src/HomeWork/Lesson3/blank.java
@@ -0,0 +1,4 @@
+package HomeWork.Lesson3;
+
+class blank {
+}
